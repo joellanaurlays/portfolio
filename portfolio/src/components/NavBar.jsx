5a7bd0e8a421e72c4bc61bbb790359466b2e3886
Joella <joellanaurlays@gmail.com>
2026-06-13 19:21:23 +0300

    Create navbar

---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faLinkedinIn,
+    faGithub,
+    faFacebookF
+} from '@fortawesome/free-brands-svg-icons'
+
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+
+export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+        window.addEventListener('scroll', onScroll);
+        return () => {
+            window.removeEventListener('scroll', onScroll);
+        };
+    }, []);
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
+    return (
+        <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <FontAwesomeIcon icon={faCode} size="2x" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle> 
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>   
+                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <a href="https://linkedin.com">
+                            <FontAwesomeIcon icon={faLinkedinIn} />
+                        </a>
+
+                        <a href="https://github.com">
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
+
+                        <a href="https://facebook.com">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </a>
+                        <button className='vvd' onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+                    </span>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
